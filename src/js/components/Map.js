b2314c6915731ad4a50f230d84943d30e02b9eeb
Cathryn Ploehn <cathryn.ploehn@nist.gov>
2015-11-05 11:39:29 -0500

    Modular overlays

--- src/js/components/Map.js
+++ src/js/components/Map.js
@@ -17,47 +17,50 @@ var styles = {
   "vectiles-water-areas" : {color: "#C6E1FF"}
 };
 
+var overlayTypes = {
+    "detectors" : { location: "data/detector_inventory.csv" }
+};
+
+
 var Overlay = React.createClass({
-  getInitialState:function(){
-    return {
-      data: null,
-      radius: 2
-    }
-  },
+    getInitialState:function(){ return { data: null, radius: 5 } },
 
-  componentDidMount:function(){
-    var thisObj = this,
-      canvas = document.getElementById(this.props.id),
-      ctxObj = canvas.getContext('2d');
+    componentDidMount:function(){
+        var thisObj = this,
+            canvas = document.getElementById(this.props.type),
+            ctxObj = canvas.getContext('2d');
 
-    d3.csv(this.props.dataLocation, function(error, lane_detectors){
-      thisObj.setState({
-        data: lane_detectors,
-        ctx: ctxObj
-      });
-    });
-  },
+        d3.csv(overlayTypes[this.props.type].location, function(error, lane_detectors){
+            thisObj.setState({
+                data: lane_detectors,
+                ctx: ctxObj
+            });
+        });
+    },
 
-  render:function(){
-    var circles = [],
-      thisObj = this;
+  render:function(){    
+    if( this.state.ctx && this.state.data ){
+      this.state.ctx.clearRect ( 0, 0, this.props.width, this.props.height );
 
-    if(this.state.ctx){
-      thisObj.state.ctx.clearRect(0,0,thisObj.props.width, thisObj.props.height);
-
-      for(key in this.state.data){
-        var longitude = +this.state.data[key].longitude;
-        var latitude = +this.state.data[key].latitude;
-        var transf = "translate(" + thisObj.props.projection([longitude, latitude]) + ")";
-        var translate = thisObj.props.projection([longitude, latitude]);
+      for( key in this.state.data ){
+        var longitude = +this.state.data[key].longitude,
+            latitude = +this.state.data[key].latitude,
+            translate = this.props.projection([longitude, latitude]);
 
         this.state.ctx.beginPath();
-        this.state.ctx.arc(translate[0],translate[1],thisObj.state.radius,0,Math.PI*2,true); 
+        this.state.ctx.arc(translate[0],translate[1],this.state.radius,0,Math.PI*2,true); 
         this.state.ctx.fill();
       }
     }
 
-    return (null)
+    return (
+        <canvas 
+            width={this.props.width}
+            height={this.props.height}
+            className={this.props.type} 
+            id={this.props.type}>      
+        </canvas>
+    )
   }
 });
 
@@ -96,12 +99,16 @@ var BackgroundLayer = React.createClass({
             thisObj.state.ctx.closePath();
 
             if (thisObj.props.type == "vectiles-highroad"){
+
                 thisObj.state.ctx.strokeStyle = style[key].color;
                 thisObj.state.ctx.lineWidth = style[key].width;
                 thisObj.state.ctx.stroke();
+
             } else {
+
                 thisObj.state.ctx.fillStyle = style.color;
                 thisObj.state.ctx.fill();
+
             }
         }
     }
@@ -190,12 +197,12 @@ var Map = React.createClass({
       // var center = [-96.65447, 33.03357]; // Dallas
 
       var projection = d3.geo.mercator()
-        .scale((1 << 22) / 2 / Math.PI)
+        .scale((1 << 18) / 2 / Math.PI)
         .translate([-(this.props.width) / 2, -(this.props.height) / 2]);  
 
       var zoomBehavior = d3.behavior.zoom()
         .scale(projection.scale() * 2 * Math.PI)
-        .scaleExtent([1 << 18, 1 << 26])
+        .scaleExtent([1 <<16, 1 << 26])
         .size([this.props.width, this.props.height])
         .translate(projection(center).map(function(x) { return -x; }))
         .on("zoom", this.zoomed);
@@ -211,14 +218,19 @@ var Map = React.createClass({
 
     zoomed:function(){
 
-      var tilesArray= this.state.tile
-        .scale(this.state.zoom.scale())
-        .translate(this.state.zoom.translate())
-        .call();
+        var tiles = this.state.tile
+            .scale(this.state.zoom.scale())
+            .translate(this.state.zoom.translate())
+            .call();
 
-      this.setState({
-        tiles: tilesArray,
-      })
+        var projection = this.state.projection
+            .translate(this.state.zoom.translate())
+            .scale(this.state.zoom.scale() / 2 / Math.PI);
+
+        this.setState({
+            tiles: tiles,
+            projection: projection
+        })
 
     },
 
@@ -233,38 +245,37 @@ var Map = React.createClass({
     },
 
     render:function(){
-      var thisObj = this, 
-        backgroundLayerTypes = ["vectiles-highroad", "vectiles-water-areas"],
-        backgroundLayers = [];
+        var thisObj = this, 
+            backgroundLayerTypes = ["vectiles-highroad", "vectiles-water-areas"],
+            backgroundLayers = [],
+            overlays = [];
 
-      for(layer in backgroundLayerTypes){
-        backgroundLayers.push(
-          <BackgroundLayer 
-            width={thisObj.props.width}
-            height={thisObj.props.height}
-            tiles={thisObj.state.tiles}
-            type={backgroundLayerTypes[layer]} >
-          </BackgroundLayer>
-        )
-      }
+        for(layer in backgroundLayerTypes){
+            backgroundLayers.push(
+              <BackgroundLayer 
+                width={thisObj.props.width}
+                height={thisObj.props.height}
+                tiles={thisObj.state.tiles}
+                type={backgroundLayerTypes[layer]} >
+              </BackgroundLayer>
+            )
+        }
+
+        for(overlay in overlayTypes){
+            overlays.push(
+                <Overlay 
+                    type={overlay}
+                    projection={this.state.projection} 
+                    width={this.props.width}
+                    height={this.props.height} >
+                </Overlay>
+            );
+        };
 
       return (
         <div className={this.state.className}>
-          <canvas 
-            width={this.props.width}
-            height={this.props.height}
-            className="detectors" 
-            id="detectors">
-              <Overlay 
-                id="detectors"
-                className="detectors"
-                projection={this.state.projection} 
-                dataLocation="data/detector_inventory.csv"
-                width={this.props.width}
-                height={this.props.height} >
-              </Overlay>
-          </canvas>
-          {backgroundLayers}
+            {overlays}
+            {backgroundLayers}
         </div>
       )
     }
